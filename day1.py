3c04e701c8e7c74550d064d91de55236c07930cb
iamshabab <iamshabab3@gmail.com>
2026-06-22 14:29:37 +0500

    Initial commit

---
+++ day1.py
@@ -0,0 +1,3 @@
+print("hello")
+print("i want to learn python")
+print("this is day 1 of my 65 days challange")
---
+++ day1_exercise.py
@@ -0,0 +1,9 @@
+print("hello my name is shabab Alam")
+print("today im learning about print function and comments")
+print('print function;')
+print('print function can be used to show something on screen')
+print("for example print(""double quotes"") or print('single quotes')we can use both single and double quotes")
+print("comments;")
+print("comments are those text ignore by python, its use for extra information or notes")
+print("comments are started from #")
+print("for example #this is comments") #this is comments we can't write inside print function
