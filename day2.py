806028f112b54a50e4a604c911f44288426d0824
iamshabab <iamshabab3@gmail.com>
2026-06-23 16:49:47 +0500

    variable

---
+++ day2.py
@@ -0,0 +1,66 @@
+#this is day 2 of 65 days chalange of learning python, ML and Deep Learning
+
+#variable
+
+name="shabab Alam"
+age="23"
+hight=5.9
+
+print(name)
+print(age)
+print(hight)
+
+#core 4 types of variable
+
+#str (string) use for text
+
+s="str is use for text variable"
+print(s)
+
+#int (intiger) is use for whole number
+
+i=5
+print(i)
+
+#float is use for decimal numbers
+
+fl=10.10
+print(fl)
+
+#bool is used for boolean (true or false) 
+
+t=True
+f=False
+
+print(t)
+print(f)
+
+#to find type of variable
+print(type(s))
+print(type(i))
+print(type(fl))
+print(type(t))
+
+
+
+#opreating math using variable
+
+a=10
+b=5
+
+print(a+b) ;print("+ add")
+print(a-b) ;print("- sub")
+print(a*b) ;print("* mul")
+print(a/b) ;print("/ dev")
+print(a//b) ;print("// floor")
+print(a%b) ;print("% riminder")
+print(a**b) ;print("** power")
+
+
+#virable can be updated any time
+age=22
+#f can be used for combining variable with print
+
+print(f"my name is {name}")
+print(f"i am {hight} feet tall")
+print(f"in 5 years i will be {age+5} years old")
---
+++ day2_exercise.py
@@ -0,0 +1,38 @@
+#exercise of day2
+
+
+name="shabab Alam"
+age=22
+hight=5.9
+student=True
+
+print(f"my name is {name}")
+print(f"im {age} years old")
+print(f"my hight is {hight}")
+print(f"is im a student? {student}")
+
+
+a=2+3
+b=3-2
+c=2*3
+d=3/2
+e=3//2
+f=3%2
+g=2**3
+print(f"2+3={a}")
+print(f"3-2={b}")
+print(f"2*2={c}")
+print(f"3/2={d}")
+print(f"3//2={e}")
+print(f"3%2={f}")
+print(f"2**3={g}")
+
+print(type(name))
+print(type(age))
+print(type(hight))
+print(type(student))
+
+
+
+
+
